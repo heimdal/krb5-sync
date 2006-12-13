aadaffb60950eccaa6bd8837bc74c1e0145df42f
Russ Allbery <rra@stanford.edu>
2006-12-13 22:30:04 +0000

    Get our own tickets, only set the error string rather than sysloging it because the kadmind patch already does the syslog, and clean up the code a bit.  This should now be self-contained and not require an external ticket cache.

--- plugin/ad.c
+++ plugin/ad.c
@@ -15,68 +15,134 @@
 
 #include <plugin/internal.h>
 
+/* The memory cache name used to store credentials for AD. */
+#define CACHE_NAME "MEMORY:krb5_sync"
+
+/*
+ * Given the plugin options, a Kerberos context, a pointer to krb5_ccache
+ * storage, and the buffer into which to store an error message if any,
+ * initialize a memory cache using the configured keytab to obtain initial
+ * credentials.  Also set the KRB5CCNAME environment variable for the use of
+ * SASL later for LDAP calls.  This is quite ugly since it changes the whole
+ * kadmind process, but it's hopefully harmless.  Return 0 on success,
+ * non-zero on failure.
+ */
+static int
+get_creds(struct plugin_config *config, krb5_context ctx, krb5_ccache *cc,
+          char *errstr, int errstrlen)
+{
+    krb5_keytab kt;
+    krb5_creds creds;
+    krb5_principal princ;
+    krb5_get_init_creds_opt opts;
+    krb5_error_code ret;
+
+    ret = krb5_kt_resolve(ctx, config->ad_keytab, &kt);
+    if (ret != 0) {
+        snprintf(errstr, errstrlen, "unable to resolve keytab \"%s\": %s",
+                 config->ad_keytab, error_message(ret));
+        return 1;
+    }
+    ret = krb5_parse_name(ctx, config->ad_principal, &princ);
+    if (ret != 0) {
+        snprintf(errstr, errstrlen, "unable to parse principal \"%s\": %s",
+                 config->ad_principal, error_message(ret));
+        return 1;
+    }
+    krb5_get_init_creds_opt_init(&opts);
+    memset(&creds, 0, sizeof(creds));
+    ret = krb5_get_init_creds_keytab(ctx, &creds, princ, kt, 0, NULL, &opts);
+    if (ret != 0) {
+        snprintf(errstr, errstrlen, "unable to get initial credentials: %s",
+                 error_message(ret));
+        return 1;
+    }
+    ret = krb5_kt_close(ctx, kt);
+    if (ret != 0) {
+        snprintf(errstr, errstrlen, "unable to close keytab: %s",
+                 error_message(ret));
+        return 1;
+    }
+    ret = krb5_cc_resolve(ctx, CACHE_NAME, cc);
+    if (ret != 0) {
+        snprintf(errstr, errstrlen, "unable to resolve memory cache: %s",
+                 error_message(ret));
+        return 1;
+    }
+    ret = krb5_cc_initialize(ctx, *cc, princ);
+    if (ret != 0) {
+        snprintf(errstr, errstrlen, "unable to initialize memory cache: %s",
+                 error_message(ret));
+        return 1;
+    }
+    ret = krb5_cc_store_cred(ctx, *cc, &creds);
+    if (ret != 0) {
+        snprintf(errstr, errstrlen, "unable to store credentials: %s",
+                 error_message(ret));
+        return 1;
+    }
+    krb5_free_cred_contents(ctx, &creds);
+    krb5_free_principal(ctx, princ);
+    return 0;
+}
+
 int
 pwupdate_ad_change(struct plugin_config *config, krb5_context ctx,
                    krb5_principal principal, char *password, int pwlen,
                    char *errstr, int errstrlen)
 {
     krb5_error_code ret;
-    char *targpname = NULL;
+    char *target = NULL;
     krb5_ccache ccache;
     int result_code;
     krb5_data result_code_string, result_string;
     int code = 0;
 
-    ret = krb5_cc_default(ctx, &ccache);
+    if (get_creds(config, ctx, &ccache, errstr, errstrlen) != 0)
+        return 1;
+
+    /* This is just for logging purposes. */
+    ret = krb5_unparse_name(ctx, principal, &target);
     if (ret != 0) {
-        if (ret == KRB5_CC_NOTFOUND)
-	    syslog(LOG_ERR, "WARNING: pwupdate failed: no kerberos credentials");
-        else 
-	    syslog(LOG_ERR, "WARNING: pwupdate failed opening kerberos ccache: %d", ret);
-	snprintf(errstr, errstrlen, "Password synchronization failure: %s", error_message(ret));
-	return (1);
+        snprintf(errstr, errstrlen, "unable to parse target principal: %s",
+                 error_message(ret));
+        return 1;
     }
 
-    ret = krb5_unparse_name(ctx, principal, &targpname);
-    if (ret != 0) {
-	syslog(LOG_ERR, "WARNING: pwupdate failed parsing target kerberos principal: %d", ret);
-	snprintf(errstr, errstrlen, "Password synchronization failure: %s", error_message(ret));
-	return (1);
-    }
-
+    /*
+     * Change the principal over to the AD realm.  Right now, this is all the
+     * rewriting or mapping that we do.  If later we need to do more
+     * comprehensive mapping, this is where we'd do it.
+     */
     krb5_set_principal_realm(ctx, principal, config->ad_realm);
-    /* If a more comprehensive rewrite function is needed, it goes here. */
 
+    /* Do the actual password change. */
     ret = krb5_set_password_using_ccache(ctx, ccache, password, principal,
                                          &result_code, &result_code_string,
                                          &result_string);
     if (ret != 0) {
-	syslog(LOG_ERR, "WARNING: pwupdate failed changing password for %s: %d", targpname, ret);
-	snprintf(errstr, errstrlen, "Password synchronization failure: %s", error_message(ret));
-	code = 1;
-	goto ret;
+        snprintf(errstr, errstrlen, "password change failed for %s in %s: %s",
+                 target, config->ad_realm, error_message(ret));
+        code = 1;
+        goto ret;
     }
-    if (result_code) {
-	syslog(LOG_ERR, "WARNING: pwupdate error changing password for %s: %.*s%s%.*s\n", targpname,
-               result_code_string.length, result_code_string.data,
-               result_string.length?": ":"",
-               result_string.length, result_string.data);
-	code = 2;
-	snprintf(errstr, errstrlen, "%.*s%s%.*s", result_code_string.length, 
-		 result_code_string.data, 
-		 result_string.length?": ":"", 
-		 result_string.length, result_string.data); 
-
-	goto ret;
+    if (result_code != 0) {
+        snprintf(errstr, errstrlen, "password change failed for %s in %s:"
+                 " %.*s%s%.*s", target, config->ad_realm,
+                 result_code_string.length, result_code_string.data, 
+                 result_string.length ? ": " : "", 
+                 result_string.length, result_string.data); 
+        code = 2;
+        goto ret;
     }
-
     free(result_string.data);
     free(result_code_string.data);
-
-    syslog(LOG_INFO, "pwupdate: %s: password changed", targpname);
+    syslog(LOG_INFO, "pwupdate: %s password changed in %s", target,
+           config->ad_realm);
     snprintf(errstr, errstrlen, "Password changed");
 
  ret:
-    krb5_free_unparsed_name(ctx, targpname);
+    krb5_free_unparsed_name(ctx, target);
+    krb5_cc_destroy(ctx, ccache);
     return code;
 }
