570123f830fc9ac599307e02d014ba20cd0a0a35
Russ Allbery <eagle@eyrie.org>
2013-12-04 22:40:43 -0800

    Clean up the setup and teardown of the queue-only test

--- tests/plugin/queuing-t.c
+++ tests/plugin/queuing-t.c
@@ -23,6 +23,7 @@
 
 #include <plugin/internal.h>
 #include <tests/tap/basic.h>
+#include <tests/tap/kerberos.h>
 #include <tests/tap/string.h>
 #include <tests/tap/sync.h>
 
@@ -30,7 +31,7 @@
 int
 main(void)
 {
-    char *tmpdir, *krb5conf, *env, *old_env;
+    char *path, *tmpdir, *krb5_config, *old_krb5_config;
     krb5_context ctx;
     krb5_principal princ;
     krb5_error_code code;
@@ -38,31 +39,37 @@ main(void)
     const char *message;
     char *wanted;
 
+    /* Define the plan. */
+    plan(32);
+
+    /* Set up a temporary directory and queue relative to it. */
     tmpdir = test_tmpdir();
     if (chdir(tmpdir) < 0)
         sysbail("cannot cd to %s", tmpdir);
-    krb5conf = test_file_path("data/default.conf");
-    if (krb5conf == NULL)
-        bail("cannot find tests/data/krb5.conf");
     if (mkdir("queue", 0777) < 0)
         sysbail("cannot mkdir queue");
-    basprintf(&env, "KRB5_CONFIG=%s", krb5conf);
-    if (putenv(env) < 0)
-        sysbail("cannot set KRB5CCNAME");
+
+    /* Point KRB5_CONFIG at the correct krb5.conf file. */
+    path = test_file_path("data/default.conf");
+    if (path == NULL)
+        bail("cannot find data/krb5.conf in the test suite");
+    basprintf(&krb5_config, "KRB5_CONFIG=%s", path);
+    if (putenv(krb5_config) < 0)
+        sysbail("cannot set KRB5_CONFIG in the environment");
+
+    /* Obtain a new Kerberos context with that krb5.conf file. */
     code = krb5_init_context(&ctx);
     if (code != 0)
-        bail("cannot create Kerberos context (%d)", (int) code);
-    code = krb5_parse_name(ctx, "test@EXAMPLE.COM", &princ);
-    if (code != 0)
-        bail("cannot parse principal: %s", krb5_get_error_message(ctx, code));
-
-    plan(32);
+        bail_krb5(ctx, code, "cannot create Kerberos context");
 
     /* Test init. */
-    is_int(0, sync_init(ctx, &data), "pwupdate_init succeeds");
+    is_int(0, sync_init(ctx, &data), "sync_init succeeds");
     ok(data != NULL, "...and data is non-NULL");
 
     /* Block processing for our test user and then test password change. */
+    code = krb5_parse_name(ctx, "test@EXAMPLE.COM", &princ);
+    if (code != 0)
+        bail_krb5(ctx, code, "cannot parse principal test@EXAMPLE.COM");
     sync_queue_block("queue", "test", "password");
     code = sync_chpass(data, ctx, princ, "foobar");
     is_int(0, code, "pwupdate_precommit_password succeeds");
@@ -103,30 +110,29 @@ main(void)
     /* Shut down the plugin. */
     sync_close(ctx, data);
     free(wanted);
-
-    /*
-     * Change to an empty Kerberos configuration file, and then make sure the
-     * plugin does nothing when there's no configuration.
-     */
     krb5_free_principal(ctx, princ);
     krb5_free_context(ctx);
-    test_file_path_free(krb5conf);
-    krb5conf = test_file_path("data/empty.conf");
-    if (krb5conf == NULL)
-        bail("cannot find tests/data/empty.conf");
-    old_env = env;
-    basprintf(&env, "KRB5_CONFIG=%s", krb5conf);
-    if (putenv(env) < 0)
+    test_file_path_free(path);
+
+    /* Change to an empty Kerberos configuration file. */
+    path = test_file_path("data/empty.conf");
+    if (path == NULL)
+        bail("cannot find data/empty.conf in the test suite");
+    old_krb5_config = krb5_config;
+    basprintf(&krb5_config, "KRB5_CONFIG=%s", path);
+    if (putenv(krb5_config) < 0)
         sysbail("cannot set KRB5CCNAME");
-    free(old_env);
+    free(old_krb5_config);
     code = krb5_init_context(&ctx);
     if (code != 0)
-        bail("cannot create Kerberos context (%d)", (int) code);
-    code = krb5_parse_name(ctx, "test@EXAMPLE.COM", &princ);
-    if (code != 0)
-        bail("cannot parse principal: %s", krb5_get_error_message(ctx, code));
+        bail_krb5(ctx, code, "cannot create Kerberos context");
+
+    /* Make sure the plugin does nothing when there's no configuration. */
     is_int(0, sync_init(ctx, &data), "sync_init succeeds");
     ok(data != NULL, "...and data is non-NULL");
+    code = krb5_parse_name(ctx, "test@EXAMPLE.COM", &princ);
+    if (code != 0)
+        bail_krb5(ctx, code, "cannot parse principal test@EXAMPLE.COM");
     code = sync_chpass(data, ctx, princ, "foobar");
     is_int(0, code, "sync_chpass succeeds");
     code = sync_status(data, ctx, princ, false);
@@ -136,10 +142,11 @@ main(void)
     /* Clean up. */
     krb5_free_principal(ctx, princ);
     krb5_free_context(ctx);
-    test_file_path_free(krb5conf);
+    putenv((char *) "KRB5_CONFIG=");
     if (chdir("..") < 0)
         sysbail("cannot chdir to parent directory");
     test_tmpdir_free(tmpdir);
-    free(env);
+    free(krb5_config);
+    test_file_path_free(path);
     return 0;
 }
