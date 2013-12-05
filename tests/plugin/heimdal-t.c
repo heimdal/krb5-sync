7b73aca0f42e10f568e35dbe16556b16c40a2dd3
Russ Allbery <eagle@eyrie.org>
2013-12-04 18:14:58 -0800

    Start cleaning up module test suite
    Import the Kerberos TAP functions from rra-c-util so that we can
    use bail_krb5, and update the Kerberos Autoconf macros to match.
    Add make-krb5-conf to build a configuration with known settings,
    although it's not being used yet.  Clean up and comment the dynamic
    loading tests and use bail more to avoid making the whole test
    conditional.

--- tests/plugin/heimdal-t.c
+++ tests/plugin/heimdal-t.c
@@ -1,6 +1,10 @@
 /*
  * Tests for the Heimdal module API.
  *
+ * This just checks that we can call all of the functions and that they return
+ * appropriate error messages for a non-existent queue.  We don't try to do
+ * any end-to-end testing of the functionality.
+ *
  * Written by Russ Allbery <eagle@eyrie.org>
  * Copyright 2012, 2013
  *     The Board of Trustees of the Leland Stanford Junior University
@@ -9,17 +13,15 @@
  */
 
 #include <config.h>
+#include <portable/kadmin.h>
 #include <portable/krb5.h>
 #include <portable/system.h>
 
 #include <dlfcn.h>
 #include <errno.h>
-#include <kadm5/admin.h>
-#ifdef HAVE_KADM5_KADM5_ERR_H
-# include <kadm5/kadm5_err.h>
-#endif
 
 #include <tests/tap/basic.h>
+#include <tests/tap/kerberos.h>
 #include <tests/tap/string.h>
 
 /*
@@ -54,7 +56,7 @@ typedef struct kadm5_hook {
 int
 main(void)
 {
-    char *krb5conf, *env, *plugin;
+    char *path, *krb5_config, *plugin;
     krb5_error_code code;
     krb5_context ctx;
     krb5_principal princ;
@@ -65,112 +67,110 @@ main(void)
     const char *message;
     char *wanted;
 
-    krb5conf = test_file_path("data/default.conf");
-    if (krb5conf == NULL)
-        bail("cannot find tests/data/krb5.conf");
-    basprintf(&env, "KRB5_CONFIG=%s", krb5conf);
-    if (putenv(env) < 0)
+    /* Set up the default krb5.conf file. */
+    path = test_file_path("data/default.conf");
+    if (path == NULL)
+        bail("cannot find data/default.conf in the test suite");
+    basprintf(&krb5_config, "KRB5_CONFIG=%s", path);
+    if (putenv(krb5_config) < 0)
         sysbail("cannot set KRB5CCNAME");
+
+    /* Obtain a Kerberos context. */
     code = krb5_init_context(&ctx);
     if (code != 0)
-        bail("cannot create Kerberos context (%d)", (int) code);
+        bail_krb5(ctx, code, "cannot initialize Kerberos context");
+
+    /* Parse a test principal into a krb5_principal structure. */
     code = krb5_parse_name(ctx, "test@EXAMPLE.COM", &princ);
     if (code != 0)
-        bail("cannot parse principal: %s", krb5_get_error_message(ctx, code));
+        bail_krb5(ctx, code, "cannot parse principal test@EXAMPLE.COM");
 
     /*
-     * We assume that the plugin is available as:
-     *
-     *     BUILD/../plugin/.libs/krb5_sync.so
-     *
-     * since we don't want to embed Libtool's libtldl just to run a test.  If
-     * that's not correct for the local platform, we skip this test.
+     * Load the module.  We assume that the plugin is available as
+     * krb5_sync.so in a .libs directory since we don't want to embed
+     * Libtool's libtldl just to run a test.  If that's not correct for the
+     * local platform, we skip this test.
      */
     plugin = test_file_path("../plugin/.libs/krb5_sync.so");
     if (plugin == NULL)
         skip_all("unknown plugin naming scheme");
-
-    plan(15);
-
-    /* Load the module and find the correct symbol. */
     handle = dlopen(plugin, RTLD_NOW);
     if (handle == NULL)
-        diag("dlopen of %s failed: %s", plugin, dlerror());
-    ok(handle != NULL, "dlopen succeeds");
-    if (handle == NULL)
-        ok(false, "dlsym succeeds");
-    else {
-        hook = dlsym(handle, "kadm5_hook_v0");
-        ok(hook != NULL, "dlsym succeeds");
-    }
+        bail("cannot dlopen %s: %s", plugin, dlerror());
+    test_file_path_free(plugin);
 
-    /* Check metadata. */
+    /* Find the dispatch table and do a basic sanity check. */
+    hook = dlsym(handle, "kadm5_hook_v0");
     if (hook == NULL)
-        ok_block(3, false, "No symbol in plugin");
-    else {
-        is_string("krb5-sync", hook->name, "Correct name");
-        is_int(KADM5_HOOK_VERSION_V0, hook->version, "Correct version");
-        is_string("Russ Allbery", hook->vendor, "Correct vendor");
-    }
+        bail("cannot get kadm5_hook_v0 symbol: %s", dlerror());
+    if (hook->init == NULL)
+        bail("no init function found in module");
+
+    /* No more skipping, so now we can report a plan. */
+    plan(13);
+
+    /* Verify the metadata. */
+    is_string("krb5-sync", hook->name, "Module name");
+    is_string("Russ Allbery", hook->vendor, "Module vendor");
+    is_int(KADM5_HOOK_VERSION_V0, hook->version, "Module version");
 
     /*
-     * Call the functions, all of which should fail with errors about queuing
-     * since the queue doesn't exist.  This verifies that the symbols are all
-     * there and that the arguments are basically correct.
+     * Call init and chpass, which should fail with errors about queuing since
+     * the queue doesn't exist.
      */
-    if (hook == NULL)
-        ok_block(8, false, "No symbol in plugin");
-    else {
-        basprintf(&wanted, "cannot open lock file queue/.lock: %s",
-                  strerror(ENOENT));
-        is_int(0, hook->init(ctx, &config), "init");
-        ok(config != NULL, "...and config is not NULL");
-        code = hook->chpass(ctx, config, KADM5_HOOK_STAGE_PRECOMMIT, princ,
-                            "test");
-        is_int(ENOENT, code, "chpass");
-        message = krb5_get_error_message(ctx, code);
-        is_string(wanted, message, "...with correct error message");
-        krb5_free_error_message(ctx, message);
+    basprintf(&wanted, "cannot open lock file queue/.lock: %s",
+              strerror(ENOENT));
+    is_int(0, hook->init(ctx, &config), "init");
+    ok(config != NULL, "...and config is not NULL");
+    code = hook->chpass(ctx, config, KADM5_HOOK_STAGE_PRECOMMIT, princ,
+                        "test");
+    is_int(ENOENT, code, "chpass");
+    message = krb5_get_error_message(ctx, code);
+    is_string(wanted, message, "...with correct error message");
+    krb5_free_error_message(ctx, message);
 
-        /* Test chpass with a NULL password. */
-        code = hook->chpass(ctx, config, KADM5_HOOK_STAGE_PRECOMMIT, princ,
-                            NULL);
-        is_int(0, code, "chpass with NULL password");
+    /* Test chpass with a NULL password, which should do nothing. */
+    code = hook->chpass(ctx, config, KADM5_HOOK_STAGE_PRECOMMIT, princ, NULL);
+    is_int(0, code, "chpass with NULL password");
 
-        /*
-         * Everything in the entity is ignored except the principal and
-         * attributes, so don't bother to fake much up here.
-         */
-        memset(&entity, 0, sizeof(entity));
-        entity.principal = princ;
-        entity.attributes = KRB5_KDB_DISALLOW_ALL_TIX;
-        code = hook->create(ctx, config, KADM5_HOOK_STAGE_PRECOMMIT, &entity,
-                            0, "test");
-        is_int(ENOENT, code, "create");
-        message = krb5_get_error_message(ctx, code);
-        is_string(wanted, message, "...with correct error message");
-        krb5_free_error_message(ctx, message);
-        code = hook->modify(ctx, config, KADM5_HOOK_STAGE_POSTCOMMIT, &entity,
-                            KADM5_ATTRIBUTES);
-        is_int(ENOENT, code, "modify");
-        message = krb5_get_error_message(ctx, code);
-        is_string(wanted, message, "...with correct error message");
-        krb5_free_error_message(ctx, message);
+    /*
+     * Set up an entry for creating an account.  Everything in the entity is
+     * ignored except the principal and attributes, so don't bother to fake
+     * much up here.
+     */
+    memset(&entity, 0, sizeof(entity));
+    entity.principal = princ;
+    entity.attributes = KRB5_KDB_DISALLOW_ALL_TIX;
 
-        /* Test create with a NULL password. */
-        code = hook->create(ctx, config, KADM5_HOOK_STAGE_PRECOMMIT, &entity,
-                            0, NULL);
-        is_int(0, code, "create with NULL password");
+    /* Test creation with no queue directory. */
+    code = hook->create(ctx, config, KADM5_HOOK_STAGE_PRECOMMIT, &entity, 0,
+                        "test");
+    is_int(ENOENT, code, "create");
+    message = krb5_get_error_message(ctx, code);
+    is_string(wanted, message, "...with correct error message");
+    krb5_free_error_message(ctx, message);
 
-        /* Close down the module. */
-        hook->fini(ctx, config);
-        free(wanted);
-    }
+    /* Test disabling with no queue directory. */
+    code = hook->modify(ctx, config, KADM5_HOOK_STAGE_POSTCOMMIT, &entity,
+                        KADM5_ATTRIBUTES);
+    is_int(ENOENT, code, "modify");
+    message = krb5_get_error_message(ctx, code);
+    is_string(wanted, message, "...with correct error message");
+    krb5_free_error_message(ctx, message);
+
+    /* Test creation with a NULL password, which should do nothing. */
+    code = hook->create(ctx, config, KADM5_HOOK_STAGE_PRECOMMIT, &entity, 0,
+                        NULL);
+    is_int(0, code, "create with NULL password");
+
+    /* Close down the module. */
+    hook->fini(ctx, config);
+    free(wanted);
 
     /* Clean up. */
     krb5_free_principal(ctx, princ);
     krb5_free_context(ctx);
-    test_file_path_free(krb5conf);
-    free(env);
+    test_file_path_free(path);
+    free(krb5_config);
     return 0;
 }
--- tests/plugin/mit-t.c
+++ tests/plugin/mit-t.c
@@ -1,6 +1,10 @@
 /*
  * Tests for the MIT Kerberos module API.
  *
+ * This just checks that we can call all of the functions and that they return
+ * appropriate error messages for a non-existent queue.  We don't try to do
+ * any end-to-end testing of the functionality.
+ *
  * Written by Russ Allbery <eagle@eyrie.org>
  * Copyright 2012, 2013
  *     The Board of Trustees of the Leland Stanford Junior University
@@ -9,20 +13,18 @@
  */
 
 #include <config.h>
+#include <portable/kadmin.h>
 #include <portable/krb5.h>
 #include <portable/system.h>
 
 #include <dlfcn.h>
 #include <errno.h>
-#include <kadm5/admin.h>
-#ifdef HAVE_KADM5_KADM5_ERR_H
-# include <kadm5/kadm5_err.h>
-#endif
 #ifdef HAVE_KRB5_KADM5_HOOK_PLUGIN_H
 # include <krb5/kadm5_hook_plugin.h>
 #endif
 
 #include <tests/tap/basic.h>
+#include <tests/tap/kerberos.h>
 #include <tests/tap/string.h>
 
 
@@ -39,131 +41,136 @@ main(void)
 int
 main(void)
 {
-    char *krb5conf, *env, *plugin;
+    char *path, *krb5_config, *plugin;
     krb5_error_code code;
     krb5_context ctx;
     krb5_principal princ;
     void *handle = NULL;
-    krb5_error_code (*callback)(krb5_context, int, int, krb5_plugin_vtable);
-    kadm5_hook_vftable_1 hook;
+    krb5_error_code (*init)(krb5_context, int, int, krb5_plugin_vtable);
+    kadm5_hook_vftable_1 vtable;
     kadm5_hook_modinfo *data = NULL;
     kadm5_principal_ent_rec entity;
     const char *message;
     char *wanted;
 
-    krb5conf = test_file_path("data/default.conf");
-    if (krb5conf == NULL)
-        bail("cannot find tests/data/krb5.conf");
-    basprintf(&env, "KRB5_CONFIG=%s", krb5conf);
-    if (putenv(env) < 0)
+    /* Set up the default krb5.conf file. */
+    path = test_file_path("data/default.conf");
+    if (path == NULL)
+        bail("cannot find data/default.conf in the test suite");
+    basprintf(&krb5_config, "KRB5_CONFIG=%s", path);
+    if (putenv(krb5_config) < 0)
         sysbail("cannot set KRB5CCNAME");
+
+    /* Obtain a Kerberos context. */
     code = krb5_init_context(&ctx);
     if (code != 0)
-        bail("cannot create Kerberos context (%d)", (int) code);
+        bail_krb5(ctx, code, "cannot initialize Kerberos context");
+
+    /* Parse a test principal into a krb5_principal structure. */
     code = krb5_parse_name(ctx, "test@EXAMPLE.COM", &princ);
     if (code != 0)
-        bail("cannot parse principal: %s", krb5_get_error_message(ctx, code));
+        bail_krb5(ctx, code, "cannot parse principal test@EXAMPLE.COM");
 
     /*
-     * We assume that the plugin is available as:
-     *
-     *     BUILD/../plugin/.libs/krb5_sync.so
-     *
-     * since we don't want to embed Libtool's libtldl just to run a test.  If
-     * that's not correct for the local platform, we skip this test.
+     * Load the module.  We assume that the plugin is available as
+     * krb5_sync.so in a .libs directory since we don't want to embed
+     * Libtool's libtldl just to run a test.  If that's not correct for the
+     * local platform, we skip this test.
      */
     plugin = test_file_path("../plugin/.libs/krb5_sync.so");
     if (plugin == NULL)
         skip_all("unknown plugin naming scheme");
-
-    plan(14);
-
-    /* Load the module and find the correct symbol. */
     handle = dlopen(plugin, RTLD_NOW);
     if (handle == NULL)
-        diag("dlopen of %s failed: %s", plugin, dlerror());
-    ok(handle != NULL, "dlopen succeeds");
-    if (handle == NULL)
-        ok(false, "dlsym succeeds");
-    else {
-        callback = dlsym(handle, "kadm5_hook_krb5_sync_initvt");
-        ok(callback != NULL, "dlsym succeeds");
-    }
+        bail("cannot dlopen %s: %s", plugin, dlerror());
+    test_file_path_free(plugin);
 
-    /* Call the callback function and get the vtable. */
-    memset(&hook, 0, sizeof(hook));
-    if (handle == NULL || callback == NULL)
-        ok(false, "callback succeeds");
-    else {
-        code = callback(ctx, 1, 0, (krb5_plugin_vtable) &hook);
-        if (code != 0)
-            diag("kadm5_hook_krb5_sync_initvt failed: %s",
-                 krb5_get_error_message(ctx, code));
-        ok(code == 0, "kadm5_hook_krb5_sync_initvt succeeds");
-    }
+    /* Find the entry point function. */
+    init = dlsym(handle, "kadm5_hook_krb5_sync_initvt");
+    if (init == NULL)
+        bail("cannot get kadm5_hook_krb5_sync_initvt symbol: %s", dlerror());
 
-    /* Check metadata. */
-    is_string("krb5_sync", hook.name, "Hook name is correct");
+    /* No more skipping, so now we can report a plan. */
+    plan(12);
+
+    /* Test for correct results when requesting the wrong API version. */
+    code = init(ctx, 2, 0, (krb5_plugin_vtable) &vtable);
+    is_int(code, KRB5_PLUGIN_VER_NOTSUPP,
+           "Correct status for bad major API version");
+
+    /* Call that function properly to get the vtable. */
+    memset(&vtable, 0, sizeof(vtable));
+    code = init(ctx, 1, 0, (krb5_plugin_vtable) &vtable);
+    if (code != 0)
+        bail_krb5(ctx, code, "cannot obtain module vtable");
+
+    /* Check that all of the expected vtable entries are present. */
+    if (vtable.init == NULL || vtable.fini == NULL || vtable.chpass == NULL
+        || vtable.create == NULL || vtable.modify == NULL)
+        bail("missing function in module vtable");
+
+    /* Verify the metadata. */
+    is_string("krb5_sync", vtable.name, "Hook name is correct");
 
     /*
-     * Call the functions, all of which should fail with errors about queuing
-     * since the queue doesn't exist.  This verifies that the symbols are all
-     * there and that the arguments are basically correct.
+     * Call the chpass function, which should fail with errors about queuing
+     * since the queue doesn't exist.
      */
-    if (hook.name == NULL)
-        ok_block(8, false, "No vtable");
-    else {
-        basprintf(&wanted, "cannot open lock file queue/.lock: %s",
-                  strerror(ENOENT));
-        is_int(0, hook.init(ctx, &data), "init");
-        ok(data != NULL, "...and data is not NULL");
-        code = hook.chpass(ctx, data, KADM5_HOOK_STAGE_PRECOMMIT, princ,
-                           false, 0, NULL, "test");
-        is_int(ENOENT, code, "chpass");
-        message = krb5_get_error_message(ctx, code);
-        is_string(wanted, message, "...with correct error message");
-        krb5_free_error_message(ctx, message);
+    basprintf(&wanted, "cannot open lock file queue/.lock: %s",
+              strerror(ENOENT));
+    is_int(0, vtable.init(ctx, &data), "init");
+    ok(data != NULL, "...and data is not NULL");
+    code = vtable.chpass(ctx, data, KADM5_HOOK_STAGE_PRECOMMIT, princ, false,
+                         0, NULL, "test");
+    is_int(ENOENT, code, "chpass");
+    message = krb5_get_error_message(ctx, code);
+    is_string(wanted, message, "...with correct error message");
+    krb5_free_error_message(ctx, message);
 
-        /* Test chpass with a NULL password. */
-        code = hook.chpass(ctx, data, KADM5_HOOK_STAGE_PRECOMMIT, princ,
-                           false, 0, NULL, NULL);
-        is_int(0, code, "chpass with NULL password");
+    /* Test chpass with a NULL password, which should do nothing. */
+    code = vtable.chpass(ctx, data, KADM5_HOOK_STAGE_PRECOMMIT, princ, false,
+                         0, NULL, NULL);
+    is_int(0, code, "chpass with NULL password");
 
-        /*
-         * Everything in the entity is ignored except the principal and
-         * attributes, so don't bother to fake much up here.
-         */
-        memset(&entity, 0, sizeof(entity));
-        entity.principal = princ;
-        entity.attributes = KRB5_KDB_DISALLOW_ALL_TIX;
-        code = hook.create(ctx, data, KADM5_HOOK_STAGE_PRECOMMIT, &entity,
-                           0, 0, NULL, "test");
-        is_int(ENOENT, code, "create");
-        message = krb5_get_error_message(ctx, code);
-        is_string(wanted, message, "...with correct error message");
-        krb5_free_error_message(ctx, message);
-        code = hook.modify(ctx, data, KADM5_HOOK_STAGE_POSTCOMMIT, &entity,
-                           KADM5_ATTRIBUTES);
-        is_int(ENOENT, code, "modify");
-        message = krb5_get_error_message(ctx, code);
-        is_string(wanted, message, "...with correct error message");
-        krb5_free_error_message(ctx, message);
+    /*
+     * Set up an entry for creating an account.  Everything in the entity is
+     * ignored except the principal and attributes, so don't bother to fake
+     * much up here.
+     */
+    memset(&entity, 0, sizeof(entity));
+    entity.principal = princ;
+    entity.attributes = KRB5_KDB_DISALLOW_ALL_TIX;
 
-        /* Test create with a NULL password. */
-        code = hook.create(ctx, data, KADM5_HOOK_STAGE_PRECOMMIT, &entity, 0,
-                           0, NULL, NULL);
-        is_int(0, code, "create with NUL password");
+    /* Test creation with no queue directory. */
+    code = vtable.create(ctx, data, KADM5_HOOK_STAGE_PRECOMMIT, &entity, 0, 0,
+                         NULL, "test");
+    is_int(ENOENT, code, "create");
+    message = krb5_get_error_message(ctx, code);
+    is_string(wanted, message, "...with correct error message");
+    krb5_free_error_message(ctx, message);
 
-        /* Close down the module. */
-        hook.fini(ctx, data);
-        free(wanted);
-    }
+    /* Test disabling with no queue directory. */
+    code = vtable.modify(ctx, data, KADM5_HOOK_STAGE_POSTCOMMIT, &entity,
+                         KADM5_ATTRIBUTES);
+    is_int(ENOENT, code, "modify");
+    message = krb5_get_error_message(ctx, code);
+    is_string(wanted, message, "...with correct error message");
+    krb5_free_error_message(ctx, message);
+
+    /* Test creation with a NULL password, which should do nothing. */
+    code = vtable.create(ctx, data, KADM5_HOOK_STAGE_PRECOMMIT, &entity, 0, 0,
+                         NULL, NULL);
+    is_int(0, code, "create with NULL password");
+
+    /* Close down the module. */
+    vtable.fini(ctx, data);
+    free(wanted);
 
     /* Clean up. */
     krb5_free_principal(ctx, princ);
     krb5_free_context(ctx);
-    test_file_path_free(krb5conf);
-    free(env);
+    test_file_path_free(path);
+    free(krb5_config);
     return 0;
 }
 
